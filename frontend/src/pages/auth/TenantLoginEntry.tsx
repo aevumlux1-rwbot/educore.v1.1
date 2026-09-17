03b1fa041513ea9b71c1af31e27221a4abbecaa2
rightware-corporations <rightware123@gmail.com>
2026-09-17 06:37:14 +0200

    Route Colégio Páscoa to dedicated tenant login

--- frontend/src/pages/auth/TenantLoginEntry.tsx
+++ frontend/src/pages/auth/TenantLoginEntry.tsx
@@ -1,8 +1,18 @@
 import { useTenant } from '@/contexts/TenantContext';
 import LoginPage from '@/pages/auth/LoginPage';
 import GenericTenantLogin from '@/pages/auth/GenericTenantLogin';
+import PascoaLogin from '@/pages/auth/PascoaLogin';
 
 export default function TenantLoginEntry() {
   const { activeTenant } = useTenant();
-  return activeTenant.slug === 'colegio-deus-connosco' ? <LoginPage /> : <GenericTenantLogin />;
+
+  if (activeTenant.slug === 'colegio-deus-connosco') {
+    return <LoginPage />;
+  }
+
+  if (activeTenant.slug === 'colegio-pascoa') {
+    return <PascoaLogin />;
+  }
+
+  return <GenericTenantLogin />;
 }
