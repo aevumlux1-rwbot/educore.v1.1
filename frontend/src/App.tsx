570c257583fcae0a2fc7d8feb70c9f71a1c5c6ab
rightware-corporations <rightware123@gmail.com>
2026-09-17 03:44:13 +0200

    Wire tenant provider into app shell

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { AuthProvider } from "@/contexts/AuthContext";
+import { TenantProvider } from "@/contexts/TenantContext";
 import { AppRouter } from "@/app/router/appRouter";
 
 const queryClient = new QueryClient();
@@ -14,9 +15,11 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <AuthProvider>
-          <AppRouter />
-        </AuthProvider>
+        <TenantProvider>
+          <AuthProvider>
+            <AppRouter />
+          </AuthProvider>
+        </TenantProvider>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
